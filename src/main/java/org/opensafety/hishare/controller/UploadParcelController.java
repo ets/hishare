34891b10fcbcc6ccb3c543a26c7171f472e126de
kylestafford <kyle.stafford@pascalmetrics.com>
2011-08-12 11:01:49 -0400

    added license to all files

--- src/main/java/org/opensafety/hishare/controller/UploadParcelController.java
+++ src/main/java/org/opensafety/hishare/controller/UploadParcelController.java
@@ -1,8 +1,27 @@
+/*******************************************************************************
+ * Copyright 2011 Pascal Metrics
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
 package org.opensafety.hishare.controller;
 
+import java.io.ByteArrayInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.UUID;
 
+import org.apache.commons.io.IOUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.opensafety.hishare.service.interfaces.http.UploadParcel;
@@ -45,25 +64,6 @@ public class UploadParcelController
 		mav = new ModelAndView("outputString");
 		String[] accessInfo = uploadParcel.uploadParcel(username, authenticationId, parcelName,
 		                                                daysToLive, payload);
-		String output = accessInfo[0]+":"+accessInfo[1];
-		mav.addObject("string", output);
-		
-		return mav;
-	}
-	
-	@RequestMapping(value="/raw", method = RequestMethod.POST)
-	public ModelAndView
-	        handleFormUpload(@RequestParam("username") String username,
-	                         @RequestParam("authenticationId") String authenticationId,
-	                         @RequestParam("parcelName") String parcelName,
-	                         @RequestParam("daysToLive") Integer daysToLive,
-	                         @RequestParam("data") String payload) throws IOException
-	{
-		ModelAndView mav;
-		
-		mav = new ModelAndView("outputString");
-		String[] accessInfo = uploadParcel.uploadParcel(username, authenticationId, parcelName,
-		                                                daysToLive, payload.getBytes());
 		String output = accessInfo[0]+":"+accessInfo[1];
 		mav.addObject("string", output);
 		
